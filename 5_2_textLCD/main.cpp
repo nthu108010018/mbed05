4b7eb14b8ccbfe88f220700104e605dcb52084bd
nthu108010018 <henry.hung.taiwan@gapp.nthu.edu.tw>
2021-03-17 02:54:56 -0700

    add main.cpp

---
+++ 5_2_textLCD/main.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+
+#include "TextLCD.h"
+
+
+DigitalOut led(LED1);      //LED1 = PA_5
+
+TextLCD lcd(D2, D3, D4, D5, D6, D7);
+
+
+int main()
+
+{
+
+      int x=30;
+
+      lcd.printf("108010018\n");
+
+      while(true)
+
+      {
+
+            led = !led;             // toggle led
+
+            lcd.locate(5,1);
+
+            lcd.printf("%5i",x);    //counter display
+
+            ThisThread::sleep_for(1s);
+            x--;
+
+      }
+
+}
